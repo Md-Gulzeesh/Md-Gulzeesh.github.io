b797f5c925ec6c07ee956a25f6946cf29f5ba1f6
Md Gulzeesh <gulzeesh2000@gmail.com>
2022-09-15 17:27:37 +0530

    home section

--- my-portfolio/src/Components/Home/Home.jsx
+++ my-portfolio/src/Components/Home/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import "./Home.css";
+import Avatar from "../../Assets/Profile_Image/linkedin-photo.jpeg";
 const Home = () => {
   return (
-    <div>
-        
-    </div>
+    <section id="home">
+      <div className="details">
+        <h1>Hi,<span className="waving" data-hover="👋"></span><br />I'm Md <span className="name">Gulzeesh</span></h1>
+        <h1>MERN Developer</h1>
+        <div className="call_section">
+          <button className="outline_btn">Resume</button><button className="solid_btn">Let's Talk</button>
+        </div>
+      </div>
+      <img src={Avatar} alt="profile" className="avatar" />
+    </section>
   );
 };
 
