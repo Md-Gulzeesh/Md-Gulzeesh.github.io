99cd7bfe911cef085cf017e3b66fbaee91313130
Md Gulzeesh <gulzeesh2000@gmail.com>
2022-09-16 19:08:56 +0530

    About added

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,16 +1,65 @@
-import './App.css';
-import About from './Components/About/About';
-import Home from './Components/Home/Home';
-import Navbar from './Components/Navbar/Navbar';
-import Projects from './Components/Projects/Projects';
-
+import "./App.css";
+import About from "./Components/About/About";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import Projects from "./Components/Projects/Projects";
+import {useEffect,useState} from "react";
 function App() {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  const mouseMove = (e) => {
+    setMousePosition({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  });
+  // disable developer mode
+  // document.onkeydown = function (e) {
+  //   if (e.keyCode == 123) {
+  //     return false;
+  //   }
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+  //     return false;
+  //   }
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+  //     return false;
+  //   }
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+  //     return false;
+  //   }
+  //   if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+  //     return false;
+  //   }
+  // };
+
+  document.addEventListener("visibilitychange", function () {
+    if (document.visibilityState === "visible") {
+      document.title = "Md Gulzeesh Portfolio";
+      window.addEventListener("mousemove", mouseMove);
+    } else {
+      document.title = "Please Come Back I'm Waiting...";
+      window.removeEventListener("mousemove", mouseMove);
+    }
+  });
   return (
     <div className="App">
-      <Home/>
-      <About/>
-      <Projects/>
-      <Navbar/>
+      <Home />
+      <About />
+      <Projects />
+      <Navbar />
+      <div
+        className="cursor"
+        style={{ left: `${mousePosition.x}px`, top: `${mousePosition.y}px` }}
+      ></div>
     </div>
   );
 }
--- my-portfolio/src/Components/Projects/Projects.jsx
+++ my-portfolio/src/Components/Projects/Projects.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import "./Project.css";
-import {AiFillGithub} from "react-icons/ai";
-import ProjectCard from './ProjectCard';
 const Projects = () => {
   return (
     <section id="project">
       <h5>My Recent Work</h5>
       <h2>Projects</h2>
       <div className="project">
+        {/* <ProjectCard/>
         <ProjectCard/>
         <ProjectCard/>
         <ProjectCard/>
         <ProjectCard/>
         <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
+        <ProjectCard/> */}
       </div>
     </section>
   );
